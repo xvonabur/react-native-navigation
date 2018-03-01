29cbf627c55973ff3bc3fbad72b06ff1371525ed
Krystof Celba <krystof.celba@icloud.com>
2018-03-01 16:52:21 +0100

    Display redbox with error even if there is not any react activity yet
    Return SplashActivity If there is not yet any react activity
    so the redbox gets displayed if error occurs.

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -15,6 +15,7 @@ import com.reactnativenavigation.utils.CompatUtils;
 
 public abstract class SplashActivity extends AppCompatActivity {
     public static boolean isResumed = false;
+    public static SplashActivity instance = null;
 
     public static void start(Activity activity) {
         Intent intent = activity.getPackageManager().getLaunchIntentForPackage(activity.getPackageName());
@@ -30,6 +31,7 @@ public abstract class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = this;
         LaunchArgs.instance.set(getIntent());
         setSplashLayout();
         IntentDataHandler.saveIntentData(getIntent());
